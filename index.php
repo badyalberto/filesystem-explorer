37e83400d8f94dfd993c78c110c18ae623b7c918
Alberto Chica <badyalberto@gmail.com>
2021-11-03 17:16:54 +0100

    table

---
+++ index.php
@@ -0,0 +1,106 @@
+<?php
+require_once('./modules/Utils.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/style.css" />
+    <link rel="stylesheet" href="./node_modules/jstree/dist/themes/default/style.min.css" />
+    <script src="./node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="./node_modules/jstree/dist/jstree.min.js"></script>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+      crossorigin="anonymous"
+      defer
+    ></script>
+  </head>
+
+  <body>
+    <header class="d-flex justify-content-center py-4">
+      <div class="header-logo">Logo</div>
+      <div>
+        <form>
+          <label>Search</label>
+          <input type="text" />
+        </form>
+      </div>
+    </header>
+
+    <main class="d-flex">
+      <aside class="w-25">
+        <ul>
+          <li>Root</li>
+          <li>Folder 1</li>
+          <li>Folder 2</li>
+        </ul>
+      </aside>
+      <article class="w-75">
+        <div class="d-flex justify-content-between">
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item"><a href="#">Library</a></li>
+              <li class="breadcrumb-item active" aria-current="page">Data</li>
+            </ol>
+          </nav>
+          <div class="me-3 d-flex justify-content-around general-button-container">
+            <button class="general-button" ><img class="general-button-img" src="./assets/img/icons/create.svg" alt="" srcset="" /></button>
+            <button class="general-button" ><img class="general-button-img" src="./assets/img/icons/upload.svg" alt="" srcset="" /></button>
+          </div>
+        </div>
+        <table class="w-100 text-center">
+          <thead>
+            <tr>
+              <th>File img</th>
+              <th>File name</th>
+              <th>Creation</th>
+              <th>Last modification</th>
+              <th>Extension</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>icon</td>
+              <td>file.txt</td>
+              <td>10/09/2020</td>
+              <td>21/09/2020</td>
+              <td><img class="type-icon" src="<?= getIcon('jpg');?>" /></td>
+              <td>
+                <span><img class="actions-button" src="./assets/img/icons/edit.svg" alt="" srcset="" /></span>
+                <span><img class="actions-button" src="./assets/img/icons/delete.svg" alt="" srcset="" /></span>
+              </td>
+            </tr>
+            <tr>
+              <td>icon</td>
+              <td>file.txt</td>
+              <td>10/09/2020</td>
+              <td>21/09/2020</td>
+              <td><img class="type-icon" src="<?= getIcon('doc');?>" /></td>
+              <td>E X</td>
+            </tr>
+            <tr>
+              <td>icon</td>
+              <td>file.txt</td>
+              <td>10/09/2020</td>
+              <td>21/09/2020</td>
+              <td><img class="type-icon" src="<?= getIcon('doc');?>" /></td>
+              <td>E X</td>
+            </tr>
+          </tbody>
+        </table>
+      </article>
+    </main>
+  </body>
+</html>
---
+++ modules/Utils.php
@@ -0,0 +1,38 @@
+<?php
+
+function getIcon($type)
+{
+    switch ($type) {
+        case 'doc':
+            return "./assets/img/icons/doc.svg";
+        case 'csv':
+            return "./assets/img/icons/csv.svg";
+        case 'jpg':
+            return "./assets/img/icons/jpg.svg";
+        case 'png':
+            return "./assets/img/icons/png.svg";
+        case 'txt':
+            return "./assets/img/icons/txt.svg";
+        case 'ppt':
+            return "./assets/img/icons/ppt.svg";
+        case 'odt':
+            return "./assets/img/icons/odt.svg";
+        case 'pdf':
+            return "./assets/img/icons/pdf.svg";
+        case 'zip':
+            return "./assets/img/icons/zip.svg";
+        case 'rar':
+            return "./assets/img/icons/rar.svg";
+        case 'exe':
+            return "./assets/img/icons/exe.svg";
+        case 'svg':
+            return "./assets/img/icons/svg.svg";
+        case 'mp3':
+            return "./assets/img/icons/mp3.svg";
+        case 'mp4':
+            return "./assets/img/icons/mp4.svg";
+        default:
+            # code...
+            break;
+    }
+}
